260ed0d26e91c3f669cc7e7b13a668440b362fc4
Wu Zhiguo <wzg547228197@gmail.com>
2021-03-18 21:43:59 +0800

    [Dubbo-7375]fix url add repeatedly (#7376)
    * fix url add repeatedly
    
    * use iterator to traverse parameters map
    
    * remove unused import
    
    * import static CommonConstants.TIMESTAMP_KEY
    
    Co-authored-by: changye <changye@startdt.com>
    (cherry picked from commit c7cee8d76bc63d8c84751770c4d49348c72fef2f)
    
    # Conflicts:
    #	dubbo-common/src/main/java/org/apache/dubbo/common/URL.java
    #	dubbo-common/src/test/java/org/apache/dubbo/common/URLTest.java

--- dubbo-common/src/test/java/org/apache/dubbo/common/URLTest.java
+++ dubbo-common/src/test/java/org/apache/dubbo/common/URLTest.java
@@ -30,6 +30,7 @@ import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -715,6 +716,27 @@ public class URLTest {
         assertEquals("context/path", url.getPath());
         assertEquals(1, url.getParameters().size());
         assertEquals("xxx", url.getParameter("application"));
+    }
+
+    @Test
+    public void testHashcode() {
+        URL url1 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
+                "dubbo-demo-api-consumer&category=consumers&check=false&dubbo=2.0.2&generic=true&interface=" +
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=1599556506417");
+        URL url2 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
+                "dubbo-demo-api-consumer&category=consumers&check=false&dubbo=2.0.2&generic=true&interface=" +
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417");
+        assertEquals(url1.hashCode(), url2.hashCode());
+
+        URL url3 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
+                "dubbo-demo-api-consumer&category=consumers&check=false&dubbo=2.0.2&interface=" +
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417");
+        assertNotEquals(url2.hashCode(), url3.hashCode());
+
+        URL url4 = URL.valueOf("consumer://30.225.20.150/org.apache.dubbo.rpc.service.GenericService?application=" +
+                "dubbo-demo-api-consumer&category=consumers&check=true&dubbo=2.0.2&interface=" +
+                "org.apache.dubbo.demo.DemoService&pid=7375&side=consumer&sticky=false&timestamp=2299556506417");
+        assertNotEquals(url3.hashCode(), url4.hashCode());
     }
 
     @Test
