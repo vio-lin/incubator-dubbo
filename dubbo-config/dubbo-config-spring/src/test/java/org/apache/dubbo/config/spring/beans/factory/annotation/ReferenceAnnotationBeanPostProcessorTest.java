apache/dubbo

[Dubbo-7367]fix too many instance bean created (#7438)
1，修复相同parameters当产生多个Reference的问题。
2，如果相同服务以不同参数订阅多次，则启动时增加WARN日志

* Update ReferenceAnnotationBeanPostProcessor.java

修改日志格式

* Update ReferenceAnnotationBeanPostProcessor.java

release referencedBeanNameIdx after used.

* Update ReferenceAnnotationBeanPostProcessorTest.java

add UT

* Update ReferenceAnnotationBeanPostProcessor.java

只处理String类型的array，对Method[]暂时不处理

* 优化generateReferenceBeanName

支持处理methods属性和arguments属性

* methods和arguments需要排序

* Update ReferenceAnnotationUtils.java

use lambda

* update ServiceInstancesChangedListener

调整ServiceInstancesChangedListener的事件通知。所有的directory都能通知到

* Update ServiceInstancesChangedListener.java

listeners内部改为HashSet

* update generateReferenceBeanName

generateReferenceBeanName改用3.0-preview逻辑

* remote println

* Update ReferenceAnnotationBeanPostProcessor.java

remote unused private method

* Update ReferenceAnnotationBeanPostProcessor.java

* Update ReferenceAnnotationBeanPostProcessorTest.java

update UT

* update UT

* revert to use ReferenceAnnotationUtils

* 使用来自kylixs的convertAttribute方法

* organize imports & update UT

* update UT

* update ReferenceAnnotationBeanPostProcessor & UT

dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/beans/factory/annotation/ReferenceAnnotationBeanPostProcessorTest.java
@@ -16,6 +16,7 @@
  */
 package org.apache.dubbo.config.spring.beans.factory.annotation;
 
+import org.apache.dubbo.config.annotation.Argument;
 import org.apache.dubbo.config.annotation.Method;
 import org.apache.dubbo.config.annotation.Reference;
 import org.apache.dubbo.config.spring.ReferenceBean;
@@ -123,6 +124,31 @@ public class ReferenceAnnotationBeanPostProcessorTest {
     @Reference
     private HelloService helloService2;
 
+    // Instance 1
+    @Reference(check = false, parameters = {"a", "2", "b", "1"}, filter = {"echo"})
+    private HelloService helloServiceWithArray0;
+
+    // Instance 2
+    @Reference(check = false, parameters = {"a", "1", "b", "2"}, filter = {"echo"})
+    private HelloService helloServiceWithArray1;
+
+    @Reference(parameters = {"b", "2", "a", "1"}, filter = {"echo"}, check = false)
+    private HelloService helloServiceWithArray2;
+
+    // Instance 3
+    @Reference(check = false, parameters = {"a", "1"}, filter = {"echo"}, methods = {@Method(name = "sayHello", timeout = 100)})
+    private HelloService helloServiceWithMethod1;
+
+    @Reference(parameters = {"a", "1"}, filter = {"echo"}, check = false, methods = {@Method(name = "sayHello", timeout = 100)})
+    private HelloService helloServiceWithMethod2;
+
+    // Instance 4
+    @Reference(parameters = {"a", "1"}, filter = {"echo"}, methods = {@Method(name = "sayHello", arguments = {@Argument(callback = true, type = "String"), @Argument(callback = false, type = "int")}, timeout = 100)}, check = false)
+    private HelloService helloServiceWithArgument1;
+
+    @Reference(check = false, filter = {"echo"}, parameters = {"a", "1"}, methods = {@Method(name = "sayHello", timeout = 100, arguments = {@Argument(callback = false, type = "int"), @Argument(callback = true, type = "String")})})
+    private HelloService helloServiceWithArgument2;
+
     @Test
     public void test() throws Exception {
 
@@ -177,7 +203,7 @@ public class ReferenceAnnotationBeanPostProcessorTest {
 
         Collection<ReferenceBean<?>> referenceBeans = beanPostProcessor.getReferenceBeans();
 
-        Assertions.assertEquals(4, referenceBeans.size());
+        Assertions.assertEquals(8, referenceBeans.size());
 
         ReferenceBean<?> referenceBean = referenceBeans.iterator().next();
 
@@ -194,7 +220,7 @@ public class ReferenceAnnotationBeanPostProcessorTest {
         Map<InjectionMetadata.InjectedElement, ReferenceBean<?>> referenceBeanMap =
                 beanPostProcessor.getInjectedFieldReferenceBeanMap();
 
-        Assertions.assertEquals(3, referenceBeanMap.size());
+        Assertions.assertEquals(10, referenceBeanMap.size());
 
         for (Map.Entry<InjectionMetadata.InjectedElement, ReferenceBean<?>> entry : referenceBeanMap.entrySet()) {
 
@@ -311,7 +337,7 @@ public class ReferenceAnnotationBeanPostProcessorTest {
 
         Collection<ReferenceBean<?>> referenceBeans = beanPostProcessor.getReferenceBeans();
 
-        Assertions.assertEquals(4, referenceBeans.size());
+        Assertions.assertEquals(8, referenceBeans.size());
 
         ReferenceBean<?> referenceBean = referenceBeans.iterator().next();
 

vio-lin/incubator-dubbo

(cherry picked from commit 23397290073381c0024706c935072dae457a5742)

dubbo-config/dubbo-config-spring/src/test/java/org/apache/dubbo/config/spring/beans/factory/annotation/ReferenceAnnotationBeanPostProcessorTest.java
@@ -109,6 +109,31 @@ public class ReferenceAnnotationBeanPostProcessorTest {
     @Reference
     private HelloService helloService2;
 
+    // Instance 1
+    @Reference(check = false, parameters = {"a", "2", "b", "1"}, filter = {"echo"})
+    private HelloService helloServiceWithArray0;
+
+    // Instance 2
+    @Reference(check = false, parameters = {"a", "1", "b", "2"}, filter = {"echo"})
+    private HelloService helloServiceWithArray1;
+
+    @Reference(parameters = {"b", "2", "a", "1"}, filter = {"echo"}, check = false)
+    private HelloService helloServiceWithArray2;
+
+    // Instance 3
+    @Reference(check = false, parameters = {"a", "1"}, filter = {"echo"}, methods = {@Method(name = "sayHello", timeout = 100)})
+    private HelloService helloServiceWithMethod1;
+
+    @Reference(parameters = {"a", "1"}, filter = {"echo"}, check = false, methods = {@Method(name = "sayHello", timeout = 100)})
+    private HelloService helloServiceWithMethod2;
+
+    // Instance 4
+    @Reference(parameters = {"a", "1"}, filter = {"echo"}, methods = {@Method(name = "sayHello", arguments = {@Argument(callback = true, type = "String"), @Argument(callback = false, type = "int")}, timeout = 100)}, check = false)
+    private HelloService helloServiceWithArgument1;
+
+    @Reference(check = false, filter = {"echo"}, parameters = {"a", "1"}, methods = {@Method(name = "sayHello", timeout = 100, arguments = {@Argument(callback = false, type = "int"), @Argument(callback = true, type = "String")})})
+    private HelloService helloServiceWithArgument2;
+
     @Test
     public void test() throws Exception {
 
@@ -163,7 +188,7 @@ public class ReferenceAnnotationBeanPostProcessorTest {
 
         Collection<ReferenceBean<?>> referenceBeans = beanPostProcessor.getReferenceBeans();
 
-        Assert.assertEquals(4, referenceBeans.size());
+        Assert.assertEquals(8, referenceBeans.size());
 
         ReferenceBean<?> referenceBean = referenceBeans.iterator().next();
 