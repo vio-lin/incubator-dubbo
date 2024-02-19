925c930cdd8c9629991e108ad5d4b857868dd939
Albumen Kevin <jhq0812@gmail.com>
2024-02-19 10:00:19 +0800

    Fix #13750, ConsistentHashLoadBalance not base on arguments (#13753)

--- dubbo-cluster/src/test/java/org/apache/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalanceTest.java
+++ dubbo-cluster/src/test/java/org/apache/dubbo/rpc/cluster/loadbalance/ConsistentHashLoadBalanceTest.java
@@ -16,18 +16,24 @@
  */
 package org.apache.dubbo.rpc.cluster.loadbalance;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.apache.dubbo.common.URL;
+import org.apache.dubbo.rpc.Invocation;
 import org.apache.dubbo.rpc.Invoker;
 import org.apache.dubbo.rpc.cluster.LoadBalance;
 import org.apache.dubbo.rpc.cluster.RouterChain;
 import org.apache.dubbo.rpc.cluster.router.state.BitList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @SuppressWarnings("rawtypes")
 class ConsistentHashLoadBalanceTest extends LoadBalanceBaseTest {
@@ -45,17 +51,45 @@ class ConsistentHashLoadBalanceTest extends LoadBalanceBaseTest {
             genericHitted, "hitted should equals to genericHitted");
     }
 
-    private Invoker findHitted(Map<Invoker,AtomicLong> invokerCounter) {
+    @Test
+    void testArgumentMatchAll() {
+        Map<Invoker, AtomicLong> counter = new ConcurrentHashMap<Invoker, AtomicLong>();
+        LoadBalance lb = getLoadBalance(ConsistentHashLoadBalance.NAME);
+        for (Invoker invoker : invokers) {
+            counter.put(invoker, new AtomicLong(0));
+        }
+        URL url = invokers.get(0).getUrl();
+
+        for (int i = 0; i < 1000; i++) {
+            Invocation invocation = mock(Invocation.class);
+            String methodName = "method1";
+            given(invocation.getMethodName()).willReturn("$invoke");
+            String[] paraTypes = new String[]{String.class.getName(), String.class.getName(), String.class.getName()};
+            Object[] argsObject = new Object[]{"arg" + i, "arg2", "arg3"};
+            Object[] args = new Object[]{methodName, paraTypes, argsObject};
+            given(invocation.getArguments()).willReturn(args);
+
+            for (int j = 0; j < 5; j++) {
+                Invoker sinvoker = lb.select(invokers, url, invocation);
+                counter.get(sinvoker).incrementAndGet();
+            }
+        }
+        for (Invoker invoker : invokers) {
+            Assertions.assertTrue(counter.get(invoker).get() > 0);
+        }
+    }
+
+    private Invoker findHitted(Map<Invoker, AtomicLong> invokerCounter) {
         Invoker invoker = null;
 
-        for (Map.Entry<Invoker,AtomicLong> entry : invokerCounter.entrySet()) {
+        for (Map.Entry<Invoker, AtomicLong> entry : invokerCounter.entrySet()) {
             if (entry.getValue().longValue() > 0) {
                 invoker = entry.getKey();
                 break;
             }
         }
 
-        Assertions.assertNotNull(invoker,"invoker should be found");
+        Assertions.assertNotNull(invoker, "invoker should be found");
 
         return null;
     }
@@ -92,8 +126,10 @@ class ConsistentHashLoadBalanceTest extends LoadBalanceBaseTest {
         Invoker<LoadBalanceBaseTest> result = lb.select(invokers, url, invocation);
 
         for (int i = 0; i < 100; i++) {
-            routerChain.setInvokers(new BitList<>(invokers), () -> {});
-            List<Invoker<LoadBalanceBaseTest>> routeInvokers = routerChain.getSingleChain(url, new BitList<>(invokers), invocation)
+            routerChain.setInvokers(new BitList<>(invokers), () -> {
+            });
+            List<Invoker<LoadBalanceBaseTest>> routeInvokers = routerChain
+                .getSingleChain(url, new BitList<>(invokers), invocation)
                 .route(url, new BitList<>(invokers), invocation);
             Invoker<LoadBalanceBaseTest> finalInvoker = lb.select(routeInvokers, url, invocation);
             Assertions.assertEquals(result, finalInvoker);
