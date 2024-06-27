apache/dubbo

do not invoke test method(#13589) (#13601)
* do not invoke test method(#13589)

* Fix style check

---------

Co-authored-by: Albumen Kevin <jhq0812@gmail.com>

dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/ExceptionFilter.java
@@ -18,6 +18,7 @@ package org.apache.dubbo.rpc.filter;
 
 import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.common.extension.DisableInject;
 import org.apache.dubbo.common.logger.ErrorTypeAwareLogger;
 import org.apache.dubbo.common.logger.LoggerFactory;
 import org.apache.dubbo.common.utils.ReflectUtils;
@@ -140,7 +141,8 @@ public class ExceptionFilter implements Filter, Filter.Listener {
     }
 
     // For test purpose
-    public void setLogger(ErrorTypeAwareLogger logger) {
+    @DisableInject
+    public void mockLogger(ErrorTypeAwareLogger logger) {
         this.logger = logger;
     }
 }

dubbo-rpc/dubbo-rpc-api/src/test/java/org/apache/dubbo/rpc/filter/ExceptionFilterTest.java
@@ -63,7 +63,7 @@ class ExceptionFilterTest {
             exceptionFilter.invoke(invoker, invocation);
         } catch (RpcException e) {
             assertEquals("TestRpcException", e.getMessage());
-            exceptionFilter.setLogger(failsafeLogger);
+            exceptionFilter.mockLogger(failsafeLogger);
             exceptionFilter.onError(e, invoker, invocation);
         }
 

vio-lin/incubator-dubbo

dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/filter/ExceptionFilter.java
@@ -18,6 +18,7 @@ package org.apache.dubbo.rpc.filter;
 
 import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.common.extension.DisableInject;
 import org.apache.dubbo.common.logger.Logger;
 import org.apache.dubbo.common.logger.LoggerFactory;
 import org.apache.dubbo.common.utils.ReflectUtils;
@@ -108,7 +109,8 @@ public class ExceptionFilter implements Filter, Filter.Listener {
     }
 
     // For test purpose
-    public void setLogger(Logger logger) {
+    @DisableInject
+    public void mockLogger(Logger logger) {
         this.logger = logger;
     }
 }

dubbo-rpc/dubbo-rpc-api/src/test/java/org/apache/dubbo/rpc/filter/ExceptionFilterTest.java
@@ -60,7 +60,7 @@ public class ExceptionFilterTest {
             exceptionFilter.invoke(invoker, invocation);
         } catch (RpcException e) {
             assertEquals("TestRpcException", e.getMessage());
-            exceptionFilter.setLogger(logger);
+            exceptionFilter.mockLogger(logger);
             exceptionFilter.onError(e, invoker, invocation);
         }
 